120be9194e87d22f124dabd2c62e65dabb506668
Vicki Whiffing <vicki.whiffing@lc-ltm003.lan>
2020-06-02 18:37:27 +0200

    Simplified welcome banner markup and styles

--- src/components/comp-welcome-banner/comp-welcome-banner.tsx
+++ src/components/comp-welcome-banner/comp-welcome-banner.tsx
@@ -8,20 +8,17 @@ import {Component, h} from "@stencil/core";
 export class CompWelcomeBanner {
   render() {
     return (
-      <div class="banner-wrapper">
-        <div class="banner">
-          <div class="banner-content-before"></div>
-          <div class="banner-content">
-            <h2>hello,</h2>
-            <h3>
-              I'm Vicki, a software engineer passionate about accessibility.
-              She/her.
-            </h3>
-          </div>
-          <div class="banner-content-spacer"></div>
-          <div class="banner-profile-background"></div>
-          <div class="banner-profile"></div>
+      <div class="banner">
+        <div class="banner-background"></div>
+        <div class="banner-content">
+          <h2>hello,</h2>
+          <h3>
+            I'm Vicki, a software engineer passionate about accessibility.
+            She/her.
+          </h3>
         </div>
+        <div class="banner-profile-background"></div>
+        <div class="banner-profile"></div>
       </div>
     );
   }
