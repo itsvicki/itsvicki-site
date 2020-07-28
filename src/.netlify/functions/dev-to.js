01fec58c37cd93434927daaccf91de5a4c95bda2
Vicki Whiffing <vicki.whiffing@lc-ltm003.lan>
2020-07-28 13:47:46 +0200

    Fixed dev to fetch

--- src/.netlify/functions/dev-to.js
+++ src/.netlify/functions/dev-to.js
@@ -1,21 +1,30 @@
-const https = require("https");
-let url = "https://dev.to/api/articles/me";
+const fetch = require("node-fetch");
+const API_ENDPOINT = "https://dev.to/api/articles/me";
 
-exports.handler = function (event, context, callback) {
-  const options = {
-    headers: {
-      api_key: `${process.env.DEV_TO_API_KEY}`,
-    },
-  };
-
-  https
-    .get(url, options, (res) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(res.body),
-      });
-    })
-    .on("error", (e) => {
-      callback(Error(e));
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(API_ENDPOINT, {
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+        api_key: `${process.env.DEV_TO_API_KEY}`,
+      },
+      method: "GET",
     });
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: data,
+      }),
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
 };
