6fa135575e1639408e0b6a5a4371e3f2c8acc771
Vicki Whiffing <vicki.whiffing@lc-ltm003.lan>
2020-07-28 11:03:31 +0200

    Test to fix error in fetch

--- src/.netlify/functions/dev-to.js
+++ src/.netlify/functions/dev-to.js
@@ -1,31 +1,18 @@
 const https = require("https");
 let url = "https://dev.to/api/articles/me";
 
-exports.handler = async function (event) {
+exports.handler = function (event, context, callback) {
   const options = {
     headers: {
       api_key: `${process.env.DEV_TO_API_KEY}`,
     },
   };
 
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, options, (res) => {
-        let data = "";
-
-        // A chunk of data has been recieved.
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        // The whole response has been received. Print out the result.
-        res.on("end", () => {
-          resolve(JSON.stringify(res.body));
-          // console.log(JSON.parse(data).explanation);
-        });
-      })
-      .on("error", (e) => {
-        reject(Error(e));
-      });
-  });
+  https
+    .get(url, options, (res) => {
+      callback(null, res.statusCode);
+    })
+    .on("error", (e) => {
+      callback(Error(e));
+    });
 };
