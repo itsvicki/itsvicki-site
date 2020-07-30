d4680700d1eb38bd4069c99d34510f34dec8af3d
Vicki Whiffing <vicki.whiffing@lc-ltm003.lan>
2020-07-30 02:53:32 +0200

    Removed component tests and added page tests

--- src/components/app-thoughts/app-thoughts.e2e.ts
+++ src/components/app-thoughts/app-thoughts.e2e.ts
@@ -1,11 +1,11 @@
 import {newE2EPage} from "@stencil/core/testing";
 
-describe("app-article", () => {
+describe("app-thoughts", () => {
   it("renders", async () => {
     const page = await newE2EPage();
-    await page.setContent("<app-article></app-article>");
+    await page.setContent("<app-thoughts></app-thoughts>");
 
-    const element = await page.find("app-article");
+    const element = await page.find("app-thoughts");
     expect(element).not.toBeNull();
     expect(element).toHaveClass("hydrated");
   });
